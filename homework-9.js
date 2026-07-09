49da86df98ccef53438d84536e34d9216acc6901
Kurbanov Muhammad <rvdhb@gmail.com>
2026-07-10 00:10:11 +0300

    Worked through array methods. №2

--- homework-9.js
+++ homework-9.js
@@ -6,61 +6,51 @@ const filteredNumbers = numbers.filter(number => number >= 5);
 
 
 const products = ["диван", "кровать", "стол", "стул", "шкаф"];
-const searchProduct = products.filter(product => product.includes("кровать"));
+const searchedProduct = products.filter(product => product.includes("кровать"));
 
 
 function reverseData(array) {
-  array.reverse();
+  return array.reverse();
 }
 
 reverseData(numbers);
 reverseData(products);
 
 
-const coments = socialComments.filter(coment => coment.email.includes(".com"));
-console.log(coments);
+const comments = socialComments.filter(comment => comment.email.includes(".com"));
+console.log(comments);
 
 
-const updatedComments = socialComments.map(coment => {
-  if (coment.id <= 5) {
-    return {...coment, postId: 2 };
-  } else {
-    return {...coment, postId: 1 };
-  }
-});
+const updatedComments = socialComments.map(comment => ({
+  ...comment,
+  postId: comment.id <= 5 ? 2 : 1
+}));
 
-const updatedComents2 = socialComments.map( coment => {
-  return {
-    id: coment.id,
-    name: coment.name
-  };
-});
+const commentUserProfiles = socialComments.map(comment => ({
+  id: comment.id,
+  name: comment.name
+}));
 
 
-
-const checkedComments = socialComments.map( coment => {
-  const checked = coment.body.length > 180;
-  return {
-    ...coment, 
-    isInvalid: checked
-  };
-});
+const validatedComments = socialComments.map(comment => ({
+  ...comment, 
+  isInvalid: comment.body.length > 180
+}));
 
 
-
-const emailsWithMap = socialComments.map( coment => {
-  return coment.email;
+const emailsWithMap = socialComments.map( comment => {
+  return comment.email;
 });
 console.log(emailsWithMap);
 
-const emailsWithReduce = socialComments.reduce((acc, coment) => {
-  acc.push(coment.email);
+const emailsWithReduce = socialComments.reduce((acc, comment) => {
+  acc.push(comment.email);
   return acc;
-},[]);
+}, []);
 console.log(emailsWithReduce);
 
 
 
-const emails = socialComments.map(coment => coment.email);
+const emails = socialComments.map(comment => comment.email);
 const stringViaToString = emails.toString();
 console.log(stringViaToString);
